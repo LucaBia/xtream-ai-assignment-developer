3dcdd909f710ec38db14f103cfea57893346eba5
LucaBia <gian.luca.99@hotmail.com>
2024-07-03 02:02:43 +0200

    refactor: reading model and predict prices

--- api.py
+++ api.py
@@ -1,54 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import numpy as np
 import pandas as pd
+import numpy as np
 import joblib
-import uvicorn
 
 app = FastAPI(title="Xtream Diamond Price Prediction API")
-
-def load_data(file):
-    """
-    Load and preprocess data from a CSV file.
-    
-    Parameters:
-        file (str): URL or local path to the CSV file containing data.
-    
-    Returns:
-        pd.DataFrame: Preprocessed DataFrame with one-hot encoded categorical variables and filtered invalid entries.
-    """
-    diamonds = pd.read_csv(file)
-    diamonds = diamonds[(diamonds.x * diamonds.y * diamonds.z != 0) & (diamonds.price > 0)]
-    for col in ['cut', 'color', 'clarity']:
-            diamonds[col] = pd.Categorical(diamonds[col])
-
-    return diamonds
-
-
-model = joblib.load("./notebooks/models/XGBoost/03-07-2024-00-53-12/model.joblib")
-
-data_file = "https://raw.githubusercontent.com/xtreamsrl/xtream-ai-assignment-engineer/main/datasets/diamonds/diamonds.csv"
-data = load_data(data_file) 
-
 
 class Diamond(BaseModel):
     carat: float
     cut: str
     color: str
     clarity: str
-    depth: float = None
-    table: float = None
-    x: float = None
-    y: float = None
-    z: float = None
+    depth: float = 0.0
+    table: float = 0.0
+    x: float = 0.0
+    y: float = 0.0
+    z: float = 0.0
 
-class SampleRequest(BaseModel):
-    cut: str
-    color: str
-    clarity: str
-    weight: float
-    n_samples: int = 5
+try:
+    model = joblib.load("./notebooks/models/XGBoost/03-07-2024-00-53-12/model.joblib")
+except FileNotFoundError:
+    raise HTTPException(status_code=500, detail="Model file not found")
 
+def prepare_features(diamond: Diamond):
+    data = {
+        'carat': [diamond.carat],
+        'depth': [diamond.depth],
+        'table': [diamond.table],
+        'x': [diamond.x],
+        'y': [diamond.y],
+        'z': [diamond.z],
+        'cut': [diamond.cut],
+        'color': [diamond.color],
+        'clarity': [diamond.clarity]
+    }
+    df = pd.DataFrame(data)
+    df = pd.get_dummies(df, columns=['cut', 'color', 'clarity'], drop_first=False)
+    
+    expected_features = model.get_booster().feature_names
+    missing_cols = set(expected_features) - set(df.columns)
+    # print(missing_cols)
+    
+    for i in missing_cols:
+        df[i] = 0
+    df = df[expected_features]
+
+    return df
+
+@app.post("/predict/")
+async def predict_price(diamond: Diamond):
+    features = prepare_features(diamond)
+    prediction = model.predict(features)
+    prediction = prediction[0].item()
+    return {"predicted_price": prediction}
 
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
